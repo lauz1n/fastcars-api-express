8e3d59226078c1cfb559d87ac8eb47e02dbe373c
Samuel <samuelcvmoura@gmail.com>
2022-11-24 13:54:27 -0300

    fixing stuff

--- backend/src/middlewares/verifytoken.js
+++ backend/src/middlewares/verifytoken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 
 module.exports = function (req, res, next) {
   //Verifying token
-  const token = req.headers.authorization?.split(" ")[1]
+  const token = req.headers.authorization.split(" ")[1]
   if (!token) return res.status(401).send("authorization")
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET)
